0b6b3bfb938fbaccc9af1b9faeea749fcf6767ca
N0rms1nk <norimskeu@proton.me>
2025-04-11 14:58:51 +0700

    Update convert_single_video.sh

--- convert_single_video.sh
+++ convert_single_video.sh
@@ -28,7 +28,7 @@ chmod +x fdkaac
 sudo mv fdkaac /usr/local/bin/fdkaac
 
 mkdir -p ~/.config/rclone
-echo "${RCLONE_CONF}" > ~/.config/rclone/rclone.conf
+echo "${RCLONE_CONF}" | base64 -d > ~/.config/rclone/rclone.conf
 
 # Prepare folders
 mkdir -p input output wav wav_stereo m4a
@@ -61,11 +61,11 @@ FILTERS=$(IFS=, ; echo "${FPS_FILTER},${SCALE_FILTER}" | sed 's/^,//;s/,$//')
 if [[ -z "$FILTERS" ]]; then
   echo "Encoding video without scaling or fps conversion..."
   ffmpeg -i "$f" -f yuv4mpegpipe -strict -1 - | \
-    x265 - --crf "$CRF" --deblock "$DEBLOCK" --rd "$RD" --y4m --output "output/${name}.hevc"
+    x265 - --crf "$CRF" --deblock "$DEBLOCK" --rd "$RD" --y4m --frame-threads 16 --pools none --log-level 2 --output-depth 8 --profile main10 --high-tier --min-cu-size 8 --ctu 64 --qg-size 32 --no-opt-cu-delta-qp --tu-intra-depth 1 --tu-inter-depth 1 --limit-tu 0 --max-tu-size 32 --me hex --subme 2 --merange 57 --no-limit-modes --no-rect --no-amp --max-merge 3 --early-skip --rskip 1 --rdpenalty 0 --no-tskip --strong-intra-smoothing --no-constrained-intra --open-gop --opt-ref-list-length-pps --keyint 250 --min-keyint 23 --bframes 4 --no-weightb --bframe-bias 0 --b-adapt 2 --b-pyramid --ref 3 --weightp --rc-lookahead 20 --slices 1 --lookahead-threads 0 --scenecut 40 --scenecut-bias 5 --qpstep 4 --qpmin 0 --qpmax 69 --opt-qp-pps --no-rc-grain --cbqpoffs -2 --crqpoffs 0 --ipratio 1.4 --pbratio 1.3 --nr-intra 0 --nr-inter 0 --no-fast-intra --no-ssim-rd --no-rd-refine --psy-rd 0.40 --no-rdoq-level --no-psy-rdoq --signhide --qcomp 0.6 --no-aq-motion --aq-mode 2 --aq-strength 0.60 --cutree --no-cu-lossless --vbv-maxrate 0 --vbv-bufsize 0 --vbv-init 0.9 --no-hrd --no-aud --info --sao --no-sao-non-deblock --no-temporal-layers --log2-max-poc-lsb 8 --no-psnr --no-ssim --no-interlace --range limited --colorprim bt709 --transfer bt709 --colormatrix bt709 --max-cll "0,0" --hdr --temporal-mvp  --no-b-intra --lookahead-slices 0 --limit-refs 0 --no-repeat-headers --output "output/${name}.hevc"
 else
   echo "Encoding video with filters: $FILTERS"
   ffmpeg -i "$f" -vf "$FILTERS" -f yuv4mpegpipe -strict -1 - | \
-    x265 - --crf "$CRF" --deblock "$DEBLOCK" --rd "$RD" --y4m --output "output/${name}.hevc"
+    x265 - --crf "$CRF" --deblock "$DEBLOCK" --rd "$RD" --y4m --frame-threads 16 --pools none --log-level 2 --output-depth 8 --profile main10 --high-tier --min-cu-size 8 --ctu 64 --qg-size 32 --no-opt-cu-delta-qp --tu-intra-depth 1 --tu-inter-depth 1 --limit-tu 0 --max-tu-size 32 --me hex --subme 2 --merange 57 --no-limit-modes --no-rect --no-amp --max-merge 3 --early-skip --rskip 1 --rdpenalty 0 --no-tskip --strong-intra-smoothing --no-constrained-intra --open-gop --opt-ref-list-length-pps --keyint 250 --min-keyint 23 --bframes 4 --no-weightb --bframe-bias 0 --b-adapt 2 --b-pyramid --ref 3 --weightp --rc-lookahead 20 --slices 1 --lookahead-threads 0 --scenecut 40 --scenecut-bias 5 --qpstep 4 --qpmin 0 --qpmax 69 --opt-qp-pps --no-rc-grain --cbqpoffs -2 --crqpoffs 0 --ipratio 1.4 --pbratio 1.3 --nr-intra 0 --nr-inter 0 --no-fast-intra --no-ssim-rd --no-rd-refine --psy-rd 0.40 --no-rdoq-level --no-psy-rdoq --signhide --qcomp 0.6 --no-aq-motion --aq-mode 2 --aq-strength 0.60 --cutree --no-cu-lossless --vbv-maxrate 0 --vbv-bufsize 0 --vbv-init 0.9 --no-hrd --no-aud --info --sao --no-sao-non-deblock --no-temporal-layers --log2-max-poc-lsb 8 --no-psnr --no-ssim --no-interlace --range limited --colorprim bt709 --transfer bt709 --colormatrix bt709 --max-cll "0,0" --hdr --temporal-mvp  --no-b-intra --lookahead-slices 0 --limit-refs 0 --no-repeat-headers --output "output/${name}.hevc"
 fi
 
 # Audio
@@ -75,6 +75,9 @@ fdkaac --profile 29 -b "$AUDIO_BITRATE" -o "m4a/${name}.m4a" "wav_stereo/${name}
 
 # Final mux
 ffmpeg -i "output/${name}.hevc" -i "m4a/${name}.m4a" -c:v copy -c:a copy "output/${name}_x265.mp4"
+
+# Cleanup
+rm "output/${name}.hevc" "wav/${name}.wav" "wav_stereo/${name}_stereo.wav"
 
 # Upload to remote
 rclone copy ./output BDoDrive:Here/Hevc --progress
